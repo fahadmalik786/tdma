ed85350393364efdecc1961ba87868a71db3bfc9
tayyabmehmood <tayyabmehmood@gmail.com>
2017-03-27 19:10:03 +0500

    fix : migration-schemas endpoint issue fixed

--- migration/support.js
+++ migration/support.js
@@ -61,6 +61,7 @@ class Utils {
 
   static getRandomScheduledSchemas(n) {
     const schemas = [];
+    const returnValue = {schemas:undefined};
     for (let i = 0; i < n; i++) {
       const schema = Utils.getRandomSchema();
 
@@ -73,7 +74,8 @@ class Utils {
         });
       }
     }
-    return schemas;
+    returnValue.schemas = schemas;
+    return returnValue;
   }
 
   static getRandomStatus() {
